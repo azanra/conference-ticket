da95bacaadbbf29a67ff4d4cd0d5f2f9bdbb2d4a
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-09 12:13:42 +0700

    Pass children in header and wrap it inside

---
+++ src/component/form.jsx
@@ -0,0 +1,11 @@
+import Header from "./header";
+
+export default function Form() {
+  return (
+    <>
+      <form>
+        <Header>Your Journey to Coding Conf 2025 Starts Here!</Header>
+      </form>
+    </>
+  );
+}
