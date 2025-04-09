f2d209b6c3a23de50c1123d772aaaa6a2aa6a013
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-09 12:20:28 +0700

    Pass children in body and wrap it

--- src/component/form.jsx
+++ src/component/form.jsx
@@ -1,10 +1,12 @@
-import Header from "./header";
+import Body from "./body.jsx";
+import Header from "./header.jsx";
 
 export default function Form() {
   return (
     <>
       <form>
         <Header>Your Journey to Coding Conf 2025 Starts Here!</Header>
+        <Body>Secure your spot at next year's biggest coding conference.</Body>
       </form>
     </>
   );
