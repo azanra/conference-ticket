eceb4ba79727ec26ced212844c5fedb29cf8d318
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-15 09:42:08 +0700

    Initialize the ticket and status state
    Pass it as a props to the form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import "./App.css";
+import Form from "./component/form.jsx";
+
+const initialTicket = {
+  avatar: null,
+  fullName: "John Doe",
+  email: "JohnDoe@gmail.com",
+  githubUsername: "@johnDoe",
+  ticketId: null,
+  date: "01/12/2002",
+};
 
 function App() {
+  const [status, setStatus] = useState("draft");
+  const [ticket, setTicket] = useState(initialTicket);
+  function handleTicket(value) {
+    setTicket(value);
+  }
   return (
     <>
-      <p>hello world</p>
+      <Form
+        ticket={ticket}
+        status={status}
+        handleTicket={handleTicket}
+        setStatus={setStatus}
+      />
     </>
   );
 }
