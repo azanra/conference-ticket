f18ed9fc311dd2102bb351c28fb6ff999f451630
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-19 18:41:06 +0700

    Contrast the text to make it easier to skim

--- src/component/body.jsx
+++ src/component/body.jsx
@@ -1,3 +1,5 @@
+import "../App.css";
+
 export default function Body({ status, ticket }) {
   const { email } = ticket;
   if (status === "draft") {
@@ -6,8 +8,9 @@ export default function Body({ status, ticket }) {
   return (
     <>
       <p>
-        We've emailed your ticket to <span>{email}</span> and will send updates
-        in the run up to the event.
+        We've emailed your ticket to{" "}
+        <span className="text-(--Gradient-text-first)">{email}</span> and will
+        send updates in the run up to the event.
       </p>
     </>
   );
